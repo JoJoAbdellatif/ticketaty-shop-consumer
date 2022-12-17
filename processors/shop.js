1eb3d47fbb4c58e69d104af9db9d5f23dd811ba8
moeyad7 <mohamed.eyad2612@gmail.com>
2022-12-17 16:45:34 +0200

    finished shop pending reservation and cancell

--- processors/shop.js
+++ processors/shop.js
@@ -1,16 +1,22 @@
 const axios = require('axios');
+const pendingURL = 'http://localhost:3000/pendingMatch'
+const reservationURL = 'http://localhost:3000/reserveMatch'
+const cancellURL = 'http://localhost:3000/cancellMatch'
 
 const processPendingTicket = async (message) => {
+  const pending =  await axios.patch(pendingURL,message.body)
   console.log('[processPendingTicket]', message)
   return Promise.resolve('[processPendingTicket]')
 };
 
 const processReservedTicket = async (message) => {
+  const resrvation =  await axios.patch(reservationURL,message.body)
   console.log('[processReservedTicket]', message)
   return Promise.resolve('[processReservedTicket]')
 };
 
 const processCancelledTicket = async (message) => {
+  const cancell =  await axios.patch(cancellURL,message.body)
   console.log('[processCancelledTicket]', message)
   return Promise.resolve('[processCancelledTicket]')
 };
