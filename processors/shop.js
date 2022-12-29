79628af079ead2fdf9aad8a4420afc14344af047
moeyad7 <mohamed.eyad2612@gmail.com>
2022-12-29 18:54:09 +0200

    added desoky code

--- processors/shop.js
+++ processors/shop.js
@@ -1,10 +1,11 @@
-const axios = require('axios'); //changed vivdly
+const axios = require('axios');
 const pendingURL = 'https://ticketaty-shop.vercel.app/pendingMatch'
 const reservationURL = 'https://ticketaty-shop.vercel.app/reserveMatch'
 const cancellURL = 'https://ticketaty-shop.vercel.app/cancellMatch'
 
+
 const processPendingTicket = async (message) => {
-  const pending =  await axios.patch(pendingURL,message.body) 
+  const pending =  await axios.patch(pendingURL,message.body)
   console.log('[processPendingTicket]', message)
   return Promise.resolve('[processPendingTicket]')
 };
@@ -21,9 +22,13 @@ const processCancelledTicket = async (message) => {
   return Promise.resolve('[processCancelledTicket]')
 };
 
+const processMasterlist = async (message) => {
+  console.log('[processMasterlist]', message)
+  return Promise.resolve('[processMasterlist]')
+};
 
 module.exports = {
   processPendingTicket,
   processReservedTicket,
-  processCancelledTicket
+  processCancelledTicket,
 };
